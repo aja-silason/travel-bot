32827fff06269eb4799c80d490e8515833b95ea3
Anania Augusto <ananiasjaimeaugusto@gmail.com>
2025-01-05 08:56:49 +0100

    feat(route): add export form to use the main route

--- src/adapter/in/server/main.ts
+++ src/adapter/in/server/main.ts
@@ -1,16 +1,13 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import { mainRoute } from "./routes/route";
 
 const app = express();
 const port = 3000;
 
-const mainRoute = {
-    route: "/api/v1/"
-}
-
 app.use(bodyParser.json()); 
 
-app.get("/", (req: Request, res: Response) => {
+app.get(mainRoute.route, (req: Request, res: Response) => {
     res.status(200).send(`https://{enviroment}/{port}${mainRoute.route}`);
 })
 
---
+++ src/adapter/in/server/routes/route.ts
@@ -0,0 +1,3 @@
+export const mainRoute = {
+    route: "/api/v1/"
+}
